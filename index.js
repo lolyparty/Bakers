30424af31e333758116e7fe27b6d47e4b46fdd10
lolyparty <ololadeolayiwola01@gmail.com>
2022-04-29 00:01:18 +0100

    updated carousel and contact and blog posts images

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-// const loadAnimation = ()=>{
-//     document.querySelector('#loadinganimation_container').classList.add('loadanimation')
-// }
+const loadAnimation = ()=>{
+    document.querySelector('#loadinganimation_container').classList.add('loadanimation')
+}
 
-// window.addEventListener('load', loadAnimation)
+window.addEventListener('load', loadAnimation)
 
-    $('.works_carousel').slick();
+    $('.works_carousel').slick({
+        centerMode:true,
+        centerPadding:'0px',
+        slidePerRow:1,
+        arrows:true,
+        dots:true,
+        slidesPerRow: 1, 
+
+    });
