3aa9e0d3234747729fa2b172c0526154499a2c35
lolyparty <ololadeolayiwola01@gmail.com>
2022-06-10 02:31:49 +0100

    updating onload eventlistener

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const loadAnimation = ()=>{
     document.querySelector('.page_container').style.display = 'block'
 }
 
-window.addEventListener('DOMContentLoaded', loadAnimation)
+window.addEventListener('load', loadAnimation)
 
 
 // anime js for naimations
